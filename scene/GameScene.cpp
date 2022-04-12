b44bd52356c77c8b4281abf2bcc0e1006f254336
ソンカイヨウ <k021g1268@g.neec.ac.jp>
2022-04-12 18:48:51 +0900

    02_03_確認課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -42,14 +42,17 @@ void GameScene::Initialize() {
 		worldTransform[i].Initialize();
 	}
 
-	// カメラ視点座標を設定
 	viewProjection.eye = {0, 0, -50};
+	viewProjection.target = {0, 0, 0};
+	viewProjection.up = {0, 1, 0};
 
-	// カメラの注視点座標を設定
-	viewProjection.target = {10, 0, 0};
+	// カメラ垂直方向視野角を設定
+	viewProjection.fovAngleY = XMConvertToRadians(45);
 
-	// カメラ上方向ベクトルを設定（右上45度指定）
-	viewProjection.up = {1, 0, 0};
+	// ニアクリップ距離
+	viewProjection.nearZ = 52.0f;
+	// ファークリップ距離
+	viewProjection.farZ = 53.0f;
 
 	// ビュープロジェクションの初期化
 	viewProjection.Initialize();
@@ -59,53 +62,76 @@ void GameScene::Update() {
 
 	// 視点移動処理
 	// 視点の移動ベクトル
-	XMFLOAT3 move = {0, 0, 0};
+	// XMFLOAT3 move = {0, 0, 0};
 
-	// 視点の移動の速さ
-	const float kEyeSpeed = 0.2f;
+	//// 視点の移動の速さ
+	// const float kEyeSpeed = 0.2f;
 
+	// if (input_->PushKey(DIK_W)) {
+	//	move = {0, 0, kEyeSpeed};
+	// } else if (input_->PushKey(DIK_S)) {
+	//	move = {0, 0, -kEyeSpeed};
+	// }
+
+	//// 視点移動（ベクトルの加算）
+	// viewProjection.eye.x += move.x;
+	// viewProjection.eye.y += move.y;
+	// viewProjection.eye.z += move.z;
+
+	// FoVの変更処理
+	// 上キーで視野角が広がる
 	if (input_->PushKey(DIK_W)) {
-		move = {0, 0, kEyeSpeed};
-	} else if (input_->PushKey(DIK_S)) {
-		move = {0, 0, -kEyeSpeed};
+		viewProjection.fovAngleY += 0.01f;
+		viewProjection.fovAngleY = min(viewProjection.fovAngleY, XM_PI);
+	}
+	// 上キーで視野角が狭まる
+	else if (input_->PushKey(DIK_S)) {
+		viewProjection.fovAngleY -= 0.01f;
+		viewProjection.fovAngleY = max(viewProjection.fovAngleY, 0.01f);
 	}
 
-	// 視点移動（ベクトルの加算）
-	viewProjection.eye.x += move.x;
-	viewProjection.eye.y += move.y;
-	viewProjection.eye.z += move.z;
-
-	// 注視点の移動処理
-	// 注視点の移動ベクトル
-	XMFLOAT3 viewTargerMove = {0, 0, 0};
-
-	// 注視点の移動の速さ
-	const float viewTargerSpeed = 0.2f;
-
-	if (input_->PushKey(DIK_RIGHT)) {
-		viewTargerMove = {viewTargerSpeed, 0, 0};
-	} else if (input_->PushKey(DIK_LEFT)) {
-		viewTargerMove = {-viewTargerSpeed, 0, 0};
+	// クリップ変更処理
+	// 上キーでニアクリップ距離を増減
+	if (input_->PushKey(DIK_UP)) {
+		viewProjection.nearZ += 0.1f;
+	} else if (input_->PushKey(DIK_DOWN)) {
+		viewProjection.nearZ -= 0.1f;
 	}
 
-	// 注視点移動（ベクトルの加算）
-	viewProjection.target.x += viewTargerMove.x;
-	viewProjection.target.y += viewTargerMove.y;
-	viewProjection.target.z += viewTargerMove.z;
+	// アスペクト比を設定
+	// viewProjection.aspectRatio = 1.0f;
 
-	// 上方向回転処理
-	// 上方向の回転速さ［ラジアン/frame］
-	const float kUpRotSpeed = 0.05f;
+	//// 注視点の移動処理
+	//// 注視点の移動ベクトル
+	// XMFLOAT3 viewTargerMove = {0, 0, 0};
 
-	// 押した方向で移動ベクトル変更
-	if (input_->PushKey(DIK_SPACE)) {
-		viewAngle += kUpRotSpeed;
-		// 2πを超えたら0に戻す
-		viewAngle = fmodf(viewAngle, XM_2PI);
-	}
+	//// 注視点の移動の速さ
+	// const float viewTargerSpeed = 0.2f;
 
-	// 上方向ベクトルを計算（半径１の円周上の座標）
-	viewProjection.up = {cosf(viewAngle), sinf(viewAngle), 0.0f};
+	// if (input_->PushKey(DIK_RIGHT)) {
+	//	viewTargerMove = {viewTargerSpeed, 0, 0};
+	// } else if (input_->PushKey(DIK_LEFT)) {
+	//	viewTargerMove = {-viewTargerSpeed, 0, 0};
+	// }
+
+	//// 注視点移動（ベクトルの加算）
+	// viewProjection.target.x += viewTargerMove.x;
+	// viewProjection.target.y += viewTargerMove.y;
+	// viewProjection.target.z += viewTargerMove.z;
+
+	//// 上方向回転処理
+	//// 上方向の回転速さ［ラジアン/frame］
+	// const float kUpRotSpeed = 0.05f;
+
+	//// 押した方向で移動ベクトル変更
+	// if (input_->PushKey(DIK_SPACE)) {
+	//	viewAngle += kUpRotSpeed;
+	//	// 2πを超えたら0に戻す
+	//	viewAngle = fmodf(viewAngle, XM_2PI);
+	// }
+
+	//// 上方向ベクトルを計算（半径１の円周上の座標）
+	// viewProjection.up = {cosf(viewAngle), sinf(viewAngle), 0.0f};
 
 	// 行列の再計算
 	viewProjection.UpdateMatrix();
@@ -121,6 +147,10 @@ void GameScene::Update() {
 	debugText_->SetPos(50, 90);
 	debugText_->Printf(
 	  "up:(%f,%f,%f)", viewProjection.up.x, viewProjection.up.y, viewProjection.up.z);
+	debugText_->SetPos(50, 110);
+	debugText_->Printf("fovAngleY(Degree):%f", XMConvertToDegrees(viewProjection.fovAngleY));
+	debugText_->SetPos(50, 130);
+	debugText_->Printf("nearZ:%f", viewProjection.nearZ);
 }
 
 void GameScene::Draw() {
