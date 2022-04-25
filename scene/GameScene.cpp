9632a3e5afd645c3686da533b3436cc22f5f8902
ソンカイヨウ <k021g1268@g.neec.ac.jp>
2022-04-25 13:17:25 +0900

    02_03_ex1

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -11,7 +11,6 @@ GameScene::~GameScene() { delete model; }
 
 void GameScene::Initialize() {
 
-
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
@@ -20,27 +19,18 @@ void GameScene::Initialize() {
 	textureHandle = TextureManager::Load("mario.jpg");
 	model = Model::Create();
 
-	// 乱数シード生成器
-	std::random_device seed_gen;
-	// メルセンヌ・ツイスターab
-	std::mt19937_64 engine(seed_gen());
-	// 乱数範囲（回転角用）
-	std::uniform_real_distribution<float> rotDist(0.0f, XM_PI);
-	// 乱数範囲（座標用）
-	std::uniform_real_distribution<float> posDist(-10.0f, 10.0f);
-
 	for (size_t i = 0; i < _countof(worldTransform); i++) {
+		for (size_t j = 0; j < _countof(worldTransform); j++) {
+			// X, Y, Z 軸周りの平行移動を設定
+			worldTransform[i][j].translation_ = {-14 + (float)i * 3.5f, 14 + (float)j * -3.5f, 0};
+			// X, Y, Z 軸周りの回転角を設定
+			worldTransform[i][j].rotation_ = {0, 0, 0};
+			// X, Y, Z 方向のスケーリングを設定
+			worldTransform[i][j].scale_ = {1, 1, 1};
 
-		// X, Y, Z 軸周りの平行移動を設定
-		worldTransform[i].translation_ = {posDist(engine), posDist(engine), posDist(engine)};
-
-		// X, Y, Z 軸周りの回転角を設定
-		worldTransform[i].rotation_ = {rotDist(engine), rotDist(engine), rotDist(engine)};
-		// X, Y, Z 方向のスケーリングを設定
-		worldTransform[i].scale_ = {1.0f, 1.0f, 1.0f};
-
-		// ワールドトランスフォームの初期化
-		worldTransform[i].Initialize();
+			// ワールドトランスフォームの初期化
+			worldTransform[i][j].Initialize();
+		}
 	}
 
 	viewProjection.eye = {0, 0, -50};
@@ -50,89 +40,40 @@ void GameScene::Initialize() {
 	// カメラ垂直方向視野角を設定
 	viewProjection.fovAngleY = XMConvertToRadians(45);
 
-	// ニアクリップ距離
-	viewProjection.nearZ = 52.0f;
-	// ファークリップ距離
-	viewProjection.farZ = 53.0f;
-
 	// ビュープロジェクションの初期化
 	viewProjection.Initialize();
 }
 
 void GameScene::Update() {
 
-	// 視点移動処理
-	// 視点の移動ベクトル
-	// XMFLOAT3 move = {0, 0, 0};
-
-	//// 視点の移動の速さ
-	// const float kEyeSpeed = 0.2f;
-
-	// if (input_->PushKey(DIK_W)) {
-	//	move = {0, 0, kEyeSpeed};
-	// } else if (input_->PushKey(DIK_S)) {
-	//	move = {0, 0, -kEyeSpeed};
-	// }
-
-	//// 視点移動（ベクトルの加算）
-	// viewProjection.eye.x += move.x;
-	// viewProjection.eye.y += move.y;
-	// viewProjection.eye.z += move.z;
-
 	// FoVの変更処理
 	// 上キーで視野角が広がる
-	if (input_->PushKey(DIK_W)) {
-		viewProjection.fovAngleY += 0.01f;
-		viewProjection.fovAngleY = min(viewProjection.fovAngleY, XM_PI);
-	}
-	// 上キーで視野角が狭まる
-	else if (input_->PushKey(DIK_S)) {
+	if (input_->PushKey(DIK_UP)) {
 		viewProjection.fovAngleY -= 0.01f;
 		viewProjection.fovAngleY = max(viewProjection.fovAngleY, 0.01f);
 	}
-
-	// クリップ変更処理
-	// 上キーでニアクリップ距離を増減
-	if (input_->PushKey(DIK_UP)) {
-		viewProjection.nearZ += 0.1f;
-	} else if (input_->PushKey(DIK_DOWN)) {
-		viewProjection.nearZ -= 0.1f;
+	// 上キーで視野角が狭まる
+	else if (input_->PushKey(DIK_DOWN)) {
+		viewProjection.fovAngleY += 0.01f;
+		viewProjection.fovAngleY = min(viewProjection.fovAngleY, XM_PI);
 	}
 
-	// アスペクト比を設定
-	// viewProjection.aspectRatio = 1.0f;
+	// 注視点の移動処理
 
-	//// 注視点の移動処理
-	//// 注視点の移動ベクトル
-	// XMFLOAT3 viewTargerMove = {0, 0, 0};
+	const float viewMoveSpd = 0.2f;
 
-	//// 注視点の移動の速さ
-	// const float viewTargerSpeed = 0.2f;
-
-	// if (input_->PushKey(DIK_RIGHT)) {
-	//	viewTargerMove = {viewTargerSpeed, 0, 0};
-	// } else if (input_->PushKey(DIK_LEFT)) {
-	//	viewTargerMove = {-viewTargerSpeed, 0, 0};
-	// }
-
-	//// 注視点移動（ベクトルの加算）
-	// viewProjection.target.x += viewTargerMove.x;
-	// viewProjection.target.y += viewTargerMove.y;
-	// viewProjection.target.z += viewTargerMove.z;
-
-	//// 上方向回転処理
-	//// 上方向の回転速さ［ラジアン/frame］
-	// const float kUpRotSpeed = 0.05f;
-
-	//// 押した方向で移動ベクトル変更
-	// if (input_->PushKey(DIK_SPACE)) {
-	//	viewAngle += kUpRotSpeed;
-	//	// 2πを超えたら0に戻す
-	//	viewAngle = fmodf(viewAngle, XM_2PI);
-	// }
-
-	//// 上方向ベクトルを計算（半径１の円周上の座標）
-	// viewProjection.up = {cosf(viewAngle), sinf(viewAngle), 0.0f};
+	if (input_->PushKey(DIK_W)) {
+		viewProjection.target.y += viewMoveSpd;
+	}
+	if (input_->PushKey(DIK_S)) {
+		viewProjection.target.y -= viewMoveSpd;
+	}
+	if (input_->PushKey(DIK_D)) {
+		viewProjection.target.x += viewMoveSpd;
+	}
+	if (input_->PushKey(DIK_A)) {
+		viewProjection.target.x -= viewMoveSpd;
+	}
 
 	// 行列の再計算
 	viewProjection.UpdateMatrix();
@@ -181,8 +122,10 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 	for (size_t i = 0; i < _countof(worldTransform); i++) {
+		for (size_t j = 0; j < _countof(worldTransform); j++) {
 
-		model->Draw(worldTransform[i], viewProjection, textureHandle);
+			model->Draw(worldTransform[i][j], viewProjection, textureHandle);
+		}
 	}
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
